84037cd6c64e7a8a1079a60cf8f0717478f56837
Alex Reynolds <thealexreynolds@gmail.com>
2013-04-11 17:31:07 -0400

    attempt to better fit view to device screen

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
+<!-- Fits view to device screen width -->
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
 <title>Orderve</title>
 
 <style>
